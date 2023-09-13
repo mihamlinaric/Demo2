f311f7b88c4fd794f5bd044d10ab15bcb361ac52
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-13 14:12:25 +0200

    Initial Commit

---
+++ Demo2Business/Demo2Business.h
@@ -0,0 +1,18 @@
+//
+//  Demo2Business.h
+//  Demo2Business
+//
+//  Created by Miha Mlinaric on 13/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Demo2Business.
+FOUNDATION_EXPORT double Demo2BusinessVersionNumber;
+
+//! Project version string for Demo2Business.
+FOUNDATION_EXPORT const unsigned char Demo2BusinessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo2Business/PublicHeader.h>
+
+
