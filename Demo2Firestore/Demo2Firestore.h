1ca55f74a5fc4d9cddc281dc7337fe9dbf45fbae
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-13 14:13:13 +0200

    Initial Commit

---
+++ Demo2Firestore/Demo2Firestore.h
@@ -0,0 +1,18 @@
+//
+//  Demo2Firestore.h
+//  Demo2Firestore
+//
+//  Created by Miha Mlinaric on 13/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Demo2Firestore.
+FOUNDATION_EXPORT double Demo2FirestoreVersionNumber;
+
+//! Project version string for Demo2Firestore.
+FOUNDATION_EXPORT const unsigned char Demo2FirestoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo2Firestore/PublicHeader.h>
+
+
