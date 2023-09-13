8fef99de0e986fe287558188be334aa82eea77e8
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-13 14:11:56 +0200

    Initial Commit

---
+++ Demo2/Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo2App.swift
+//  Demo2
+//
+//  Created by Miha Mlinaric on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
