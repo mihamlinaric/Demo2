3b818b41c46e991c2af5e0b9ded5bc19bb97849e
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-14 15:31:24 +0200

    upam da dela

---
+++ Demo2Utility/Constants/FirebaseStorageConstants.swift
@@ -0,0 +1,12 @@
+//
+//  FirebaseStorageConstants.swift
+//  Demo2Utility
+//
+//  Created by Miha Mlinaric on 14/09/2023.
+//
+
+import Foundation
+
+public struct FirebaseStorageConstants {
+    public static let profileImage = "/profile_images/"
+}
