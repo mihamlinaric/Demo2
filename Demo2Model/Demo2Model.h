f6c47c231530036abb77c1db092955ca7724c363
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-14 08:52:46 +0200

    Initial Commit

---
+++ Demo2Model/Demo2Model.h
@@ -0,0 +1,18 @@
+//
+//  Demo2Model.h
+//  Demo2Model
+//
+//  Created by Miha Mlinaric on 14/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Demo2Model.
+FOUNDATION_EXPORT double Demo2ModelVersionNumber;
+
+//! Project version string for Demo2Model.
+FOUNDATION_EXPORT const unsigned char Demo2ModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo2Model/PublicHeader.h>
+
+
