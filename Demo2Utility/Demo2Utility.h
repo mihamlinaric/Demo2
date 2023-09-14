d28f6cddf40d7fbf4da4f9b9ae62c53bed01142a
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-14 11:51:28 +0200

    Initial Commit

---
+++ Demo2Utility/Demo2Utility.h
@@ -0,0 +1,18 @@
+//
+//  Demo2Utility.h
+//  Demo2Utility
+//
+//  Created by Miha Mlinaric on 14/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Demo2Utility.
+FOUNDATION_EXPORT double Demo2UtilityVersionNumber;
+
+//! Project version string for Demo2Utility.
+FOUNDATION_EXPORT const unsigned char Demo2UtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo2Utility/PublicHeader.h>
+
+
