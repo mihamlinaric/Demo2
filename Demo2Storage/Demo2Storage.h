3361a599886c8ec14d387c88d6865dcacc6b3ff5
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-14 10:16:18 +0200

    Initial Commit

---
+++ Demo2Storage/Demo2Storage.h
@@ -0,0 +1,18 @@
+//
+//  Demo2Storage.h
+//  Demo2Storage
+//
+//  Created by Miha Mlinaric on 14/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Demo2Storage.
+FOUNDATION_EXPORT double Demo2StorageVersionNumber;
+
+//! Project version string for Demo2Storage.
+FOUNDATION_EXPORT const unsigned char Demo2StorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo2Storage/PublicHeader.h>
+
+
