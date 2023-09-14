5450653abaf2824471d8eb6ccf7e51d76e6a6202
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-14 15:31:24 +0200

    upam da dela

--- Demo2Storage/Service/ImageUploader.swift
+++ Demo2Storage/Service/ImageUploader.swift
@@ -10,10 +10,10 @@ import UIKit
 import FirebaseStorage
 
 public struct ImageUploader {
-    public static func uploadImage(image: UIImage) async throws -> String? {
+    public static func uploadImage(image: UIImage, storagePath: String) async throws -> String? {
         guard let imageData = image.jpegData(compressionQuality: 0.5) else { return nil }
         let filename = NSUUID().uuidString
-        let ref = Storage.storage().reference(withPath: "/profile_images/\(filename)")
+        let ref = Storage.storage().reference(withPath: "\(storagePath)\(filename)")
         
         let _ = try await ref.putDataAsync(imageData)
         let url = try await ref.downloadURL()
