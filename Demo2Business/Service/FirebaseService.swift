40cd900a58e7d2de15c9d4150da0aaad42f47725
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-15 08:56:12 +0200

    moved firebase config to demo2business

--- Demo2Business/Service/FirebaseService.swift
+++ Demo2Business/Service/FirebaseService.swift
@@ -5,3 +5,21 @@
 //  Created by Miha Mlinaric on 14/09/2023.
 //
 
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+public class FirebaseService {
+    
+    public static func _config() {
+        @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    }
+}
