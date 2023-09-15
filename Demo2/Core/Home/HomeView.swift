6d875858b4095c0d0b907a84eb636803066da778
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-15 08:38:25 +0200

    fixed double firestore dependency problem, one i sin Demo2 and one is in Demo2Firestore

---
+++ Demo2/Core/Home/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  Demo2
+//
+//  Created by Miha Mlinaric on 14/09/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
