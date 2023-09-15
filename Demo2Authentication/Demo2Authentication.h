74cf32234ebce85573902b5f4f9d8b6236acb411
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-15 13:32:00 +0200

    Initial Commit

---
+++ Demo2Authentication/Demo2Authentication.h
@@ -0,0 +1,18 @@
+//
+//  Demo2Authentication.h
+//  Demo2Authentication
+//
+//  Created by Miha Mlinaric on 15/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Demo2Authentication.
+FOUNDATION_EXPORT double Demo2AuthenticationVersionNumber;
+
+//! Project version string for Demo2Authentication.
+FOUNDATION_EXPORT const unsigned char Demo2AuthenticationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo2Authentication/PublicHeader.h>
+
+
