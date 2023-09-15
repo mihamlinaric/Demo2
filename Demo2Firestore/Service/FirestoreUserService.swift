3fdc77c88b89e85117111511b47867c52397b84d
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-15 13:15:29 +0200

    refactored Users UI, return user after creating it

--- Demo2Firestore/Service/FirestoreUserService.swift
+++ Demo2Firestore/Service/FirestoreUserService.swift
@@ -11,21 +11,32 @@ import FirebaseFirestoreSwift
 import Demo2Model
 
 public struct FirestoreUserService: FirestoreUserProtocols {
-    
     public init() { }
     
     public func fetchUserData(_ uid: String) async throws -> User? {
         let snapshot = try await FirestoreConstants.usersCollection.document(uid).getDocument()
         return try? snapshot.getData(as: User.self)
     }
+    
     public func fetchAllUsersData() async throws -> [User] {
         let snapshot = try await FirestoreConstants.usersCollection.getDocuments()
         return snapshot.getData(as: User.self)
     }
-    public func createUser(username: String, age: Int, imageUrl: String?) async throws {
+    
+    public func createUser(username: String, age: Int, imageUrl: String?) async throws -> User? {
         let ref = FirestoreConstants.usersCollection.document()
         let user = User(id: ref.documentID, username: username, age: age, imageUrl: imageUrl)
-        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return nil }
         try await ref.setData(encodedUser)
+        
+        return user
+    }
+    
+    public func updateUserData(_ uid: String, data: [String : Any]) async throws {
+        try await FirestoreConstants.usersCollection.document(uid).updateData(data)
+    }
+    
+    public func removeUserData(_ uid: String) async throws {
+        try await FirestoreConstants.usersCollection.document(uid).delete()
     }
 }
