21f2d4796ea2e0a72786f167817f8cf47e56c306
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-18 09:47:03 +0200

    refactored users, added auth

--- Demo2Model/Model/User.swift
+++ Demo2Model/Model/User.swift
@@ -8,29 +8,31 @@
 import Foundation
 
 public struct User: Codable, Identifiable, Hashable {
-    public var id: String
-    public let username: String
+    public let id: String
+    public var username: String
     public var age: Int
+    public let email: String
     public var imageUrl: String?
     
     private enum CodingKeys: String, CodingKey {
-            case id, username, age, imageUrl
+            case id, username, age, email, imageUrl
     }
     
-    public init(id: String, username: String, age: Int, imageUrl: String? = nil) {
+    public init(id: String, username: String, age: Int, email: String, imageUrl: String? = nil) {
         self.id = id
         self.username = username
         self.age = age
+        self.email = email
         self.imageUrl = imageUrl
     }
 }
 
 extension User {
     public static let MOCK_USERS: [User] = [
-        .init(id: NSUUID().uuidString, username: "user1", age: 1),
-        .init(id: NSUUID().uuidString, username: "user2", age: 2),
-        .init(id: NSUUID().uuidString, username: "user3", age: 3),
-        .init(id: NSUUID().uuidString, username: "user4", age: 4),
-        .init(id: NSUUID().uuidString, username: "user5", age: 5)
+        .init(id: NSUUID().uuidString, username: "user1", age: 1, email: "user1@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "user2", age: 2, email: "user2@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "user3", age: 3, email: "user3@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "user4", age: 4, email: "user4@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "user5", age: 5, email: "user5@gmail.com")
     ]
 }
