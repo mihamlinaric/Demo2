7c44ca2ffb713e2e39462947c2e4db6fe86c20e4
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-18 09:47:02 +0200

    refactored users, added auth

--- Demo2Authentication/Service/AuthenticationService.swift
+++ Demo2Authentication/Service/AuthenticationService.swift
@@ -12,15 +12,30 @@ import Demo2Model
 public class AuthenticationService {
     public init() { }
     
+    /// instance that tells us if user is logged in or not
+    @Published public var userSession: FirebaseAuth.User?
+    /// instance of our user object
     @Published public var currentUser: Demo2Model.User?
     
     @MainActor
-    /// logs user in using FirebaseAuth
-    /// - Parameters:
-    ///   - email: instance of email String
-    ///   - password: instance of  password String
     public func login(withEmail email: String, password: String) async throws {
         let result = try await Auth.auth().signIn(withEmail: email, password: password)
-//            try await loadUserData()
+        self.userSession = result.user
+    }
+    
+    public func createUser(withEmail email: String, password: String) async throws {
+        let result = try await Auth.auth().createUser(withEmail: email, password: password)
+        self.userSession = result.user
+    }
+    
+    public func removeAccount() throws {
+        Auth.auth().currentUser?.delete()
+        try self.signOut()
+    }
+    
+    public func signOut() throws {
+        try Auth.auth().signOut()
+        self.userSession = nil
+        self.currentUser = nil
     }
 }
