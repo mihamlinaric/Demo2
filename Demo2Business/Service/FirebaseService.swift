a3e6ed20a62390021cd9690ae7270d349c85657f
Miha Mlinaric <miha.mlinaric@inova.si>
2023-09-20 09:01:52 +0200

    moved firebase package dependencies to top level, works on actual device

--- Demo2Business/Service/FirebaseService.swift
+++ Demo2Business/Service/FirebaseService.swift
@@ -5,22 +5,22 @@
 //  Created by Miha Mlinaric on 14/09/2023.
 //
 
-import SwiftUI
-import FirebaseCore
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
-
-public class FirebaseService {
-    public init() { }
-    
-    public func configure() {
-        @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    }
-}
+//import SwiftUI
+//import FirebaseCore
+//
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
+//
+//public class FirebaseService {
+//    public init() { }
+//
+//    public func configure() {
+//        @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//    }
+//}
